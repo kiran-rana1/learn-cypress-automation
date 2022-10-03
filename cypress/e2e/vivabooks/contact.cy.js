f2f9a0b10c361c643309902349299245ccc3e49f
kiran-rana1 <pvr8971@gmail.com>
2022-10-03 17:38:27 +1100

    added contact validation

---
+++ cypress/e2e/vivabooks/contact.cy.js
@@ -0,0 +1,26 @@
+import address from '../../fixtures/address.json'
+
+
+describe('visit contact us page and validate its functionality', () => {
+    it('visit contact us page and validate its content', () => {
+        cy.visit('https://www.vivabooksindia.com/')
+        cy.contains('a', 'Contact Us').click()
+        cy.url().should('include', '/contact-us')
+
+        Object.keys(address).forEach(function (key) {
+            cy.contains(address[key]).should('be.visible')
+        });
+
+    });
+
+    it('fill contact form and submit', () => {
+        cy.get('#con_name').type('abhijeet')
+        cy.get('#con_email').type('k@gmail.com')
+        cy.get('#con_phone').type('123456')
+        cy.get('#con_message').type('hi welcome')
+        cy.get('#submit').click()
+
+        cy.get('.alert-success').should('contain.text', 'Your enquiry is successfully sent')
+
+    });
+});
