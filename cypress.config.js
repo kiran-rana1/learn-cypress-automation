7fdab7ef7b11ff3e8f64f232ccad51dfe5bed075
kiran-rana1 <pvr8971@gmail.com>
2022-10-12 16:44:34 +1100

    login vivabooks

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 require("dotenv").config()
 
 module.exports = defineConfig({
+  viewportWidth: 1000,
+  viewportHeight: 1000,
+  redirectionLimit: 1000,
   defaultCommandTimeout: 30000,
   e2e: {
     baseUrl: process.env.BASE_URL,
--- cypress/e2e/vivabooks/test.cy.js
+++ cypress/e2e/vivabooks/test.cy.js
@@ -1,3 +1,15 @@
-const { faker } = require('@faker-js/faker')
+const address = {
+    "companyName": "Viva Books Private Limited",
+    "addressLine1": "4737/23, Ansari Road, Daryaganj",
+    "addressLine2": "New Delhi 110002",
+    "Tel": "011 422 422 00",
+    "Fax": "0091 11 422 422 40",
+    "Email": "viva@vivagroupindia.in"
+}
 
-console.log(faker.name.fullName())
+
+
+
+Object.entries(address).forEach(([key, value]) => {
+    console.log(`${key} ***** ${value}`);
+});
